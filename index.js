e53ea1c58dd14c8fdc704d4a0b9a5b1d6103eb96
hand-dot <kyouhei.fukuda0729@gmail.com>
2019-06-11 00:55:04 +0900

    2 files modified

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import React from 'react';
-import debounce from 'lodash/debounce';
 import Handsontable from 'handsontable';
 import 'handsontable/dist/handsontable.full.min.css';
 import { Controlled as CodeMirror } from 'react-codemirror2';
