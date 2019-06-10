fa51eaf414d4875b7650b6eb6c5a556c31c4d965
hand-dot <kyouhei.fukuda0729@gmail.com>
2019-06-10 23:25:34 +0900

    2 files modified

--- index.js
+++ index.js
@@ -7,17 +7,20 @@ import 'handsontable/dist/handsontable.full.min.css';
 import { Controlled as CodeMirror } from 'react-codemirror2';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/material.css';
+import 'codemirror/mode/markdown/markdown';
 import markdownTable from 'markdown-table';
 import Remarkable from "remarkable";
-// import hljs from 'highlight.js/lib/highlight';
-import 'highlight.js/styles/github.css';
+import './style.css';
 
 const editorOptions = {
-  height: '600px',
-  mode: 'markdown',
+  mode: "gfm",
   theme: 'material',
   lineNumbers: true,
 };
+
+const get2DArray = (rows, columns, initial = '') => {
+  return JSON.parse(JSON.stringify((new Array(rows)).fill((new Array(columns)).fill(initial))))
+}
 
 class App extends Component {
   constructor(props) {
@@ -25,6 +28,8 @@ class App extends Component {
     this.table = null;
     this.code = null;
     this.state = {
+      columns: 5,
+      rows: 10,
       markdown: 'hello'
     };
   }
@@ -32,6 +37,8 @@ class App extends Component {
   componentDidMount() {
     const self = this;
     this.hot = new Handsontable(this.table, {
+      columns: [...Array(self.state.columns)],
+      data: get2DArray(self.state.rows, self.state.columns),
       afterChange(changes, source) {
         if (source === 'code' || !changes) return;
         const sourceDataArray = self.hot.getSourceDataArray();
@@ -43,51 +50,37 @@ class App extends Component {
   }
 
   getRawMarkup() {
-    var md = new Remarkable('full', {
-      html: false,        // Enable HTML tags in source
-      xhtmlOut: false,        // Use '/' to close single tags (<br />)
-      breaks: false,        // Convert '\n' in paragraphs into <br>
-      langPrefix: 'language-',  // CSS language prefix for fenced blocks
-      linkify: true,         // autoconvert URL-like texts to links
-      linkTarget: '',           // set target to open link in
-
-      // Enable some language-neutral replacements + quotes beautification
-      typographer: false,
-
-      // Double + single quotes replacement pairs, when typographer enabled,
-      // and smartquotes on. Set doubles to '«»' for Russian, '„“' for German.
-      quotes: '“”‘’',
-
-      // Highlighter function. Should return escaped HTML,
-      // or '' if input not changed
-      highlight: function (str, lang) {
-        if (lang && hljs.getLanguage(lang)) {
-          try {
-            return hljs.highlight(lang, str).value;
-          } catch (__) { }
-        }
-
-        try {
-          return hljs.highlightAuto(str).value;
-        } catch (__) { }
-
-        return ''; // use external default escaping
-      }
-    });
+    var md = new Remarkable();
     return { __html: md.render(this.state.markdown) };
   }
-
+  handleInput(input, e) {
+    const value = Number(e.target.value);
+    if (value < 1) return;
+    this.setState({ [input]: value });
+  }
 
   render() {
-    const { markdown } = this.state;
+    const { columns, rows, markdown } = this.state;
     return (
       <div>
+        <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+          <div>
+            <label htmlFor="columns">columns</label>
+            <input onChange={this.handleInput.bind(this, 'columns')} id="columns" value={columns} type="number" />
+          </div>
+          <span>x</span>
+          <div>
+            <label htmlFor="rows">rows</label>
+            <input onChange={this.handleInput.bind(this, 'rows')} id="rows" value={rows} type="number" />
+          </div>
+        </div>
         <div ref={(node) => { this.table = node; }} />
         <CodeMirror
           value={markdown}
           options={editorOptions}
         />
         <div
+          className="preview"
           dangerouslySetInnerHTML={this.getRawMarkup()}
         />
       </div>
